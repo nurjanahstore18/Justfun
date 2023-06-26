7a1bc9301d08884f954c08e69cea7d4eaed9fac0
ISTIQFAR AMALINA <106859380+nurjanahstore18@users.noreply.github.com>
2023-06-26 21:06:15 +0700

    Create 160x600.js

---
+++ 160x600.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : 'cf4a601b4f9205435673f80c30995042',
+		'format' : 'iframe',
+		'height' : 600,
+		'width' : 160,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/cf4a601b4f9205435673f80c30995042/invoke.js"></scr' + 'ipt>');
