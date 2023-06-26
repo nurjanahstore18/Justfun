44db6529fd42412d9731e9ace8ea9a8009ac40a2
ISTIQFAR AMALINA <106859380+nurjanahstore18@users.noreply.github.com>
2023-06-26 21:05:15 +0700

    Create 250x300.js

---
+++ 250x300.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '29fbca2593108df41c19dff97137f123',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/29fbca2593108df41c19dff97137f123/invoke.js"></scr' + 'ipt>');
